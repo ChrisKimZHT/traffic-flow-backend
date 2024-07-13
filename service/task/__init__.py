849d4c0068461c0e7665e2a40b072137740c8428
ChrisKim <1515748548@qq.com>
2024-07-13 14:36:19 +0800

    [feat] 输出log接口

--- service/task/__init__.py
+++ service/task/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .list import list_router
+from .log import log_router
 from .run_statistic import run_statistic_router
 
 task_router = APIRouter()
 
 task_router.include_router(list_router)
 task_router.include_router(run_statistic_router)
+task_router.include_router(log_router)
---
+++ service/task/log.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from crud.TaskCrud import TaskCrud
+from util.get_db import get_db
+
+log_router = APIRouter()
+
+
+@log_router.get("/log")
+async def _(taskId: int, db: Session = Depends(get_db)):
+    try:
+        task = TaskCrud.get_by_id(db, taskId)
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"status": 1, "message": f"Database Error: {e}"})
+
+    if task is None:
+        return JSONResponse(status_code=404, content={"status": 1, "message": "Task not found"})
+
+    output_file = task.output_file
+
+    with open(output_file, "r", encoding="utf-8") as f:
+        log = f.read()
+
+    return JSONResponse(status_code=200, content={"status": 0, "message": "OK", "data": log})
